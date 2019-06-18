0508705ef6a59b86779d6ff51faa12df5075e296
Kingsley Sun <suz@tradeshift.com>
2019-06-18 10:24:04 +0800

    copy channel block sh

---
+++ basic-network/copy_channel_block_here.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error, print all commands.
+set -ev
+
+scp suz@10.133.253.41:~/fabric-samples/basic-network/channel/mychannel.block ~/fabric-samples/basic-network/channel
