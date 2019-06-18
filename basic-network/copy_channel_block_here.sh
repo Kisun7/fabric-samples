11045196ced56c52a47293d87c393922e4804d44
Kingsley Sun <suz@tradeshift.com>
2019-06-18 15:40:08 +0800

    update

--- basic-network/copy_channel_block_here.sh
+++ basic-network/copy_channel_block_here.sh
@@ -7,4 +7,5 @@
 # Exit on first error, print all commands.
 set -ev
 
+rm -rf ~/fabric-samples/basic-network/channel/mychannel.block
 scp suz@10.133.253.41:~/fabric-samples/basic-network/channel/mychannel.block ~/fabric-samples/basic-network/channel
